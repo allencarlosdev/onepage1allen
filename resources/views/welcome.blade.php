c2b80464f61849e08d6bc38a69be92aa7d541b59
Carlos Allen <allencarlosDev@gmail.com>
2022-01-20 22:42:30 -0400

    HTML STRUCTURE - section our design tecniques

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,21 @@
             <h2>AllenCarlosDesigner</h2>
         </section>
         <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path></svg></div>
-
+        <main>
+            <section class="container about-me"></section>
+                <h2 class="title"> Our design techniques</h2>
+                <div class="container-about-me">
+                    <img src="img/ourtecniquesallencarlos.svg" alt="" class="image-our-tecniques">
+                    <div class="container-texts">
+                        <h3 class=""><span>1</span> The best design techniques </h3>
+                        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus veritatis minima ipsam. Accusantium eum, debitis vero laudantium officiis ut eligendi odio, corporis dolores dignissimos, repudiandae dolorum itaque facere magnam. Sapiente!</p>
+                        <h3 class=""><span>2</span> The largest community of designers </h3>
+                        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus veritatis minima ipsam. Accusantium eum, debitis vero laudantium officiis ut eligendi odio, corporis dolores dignissimos, repudiandae dolorum itaque facere magnam. Sapiente!</p>
+                        <h3 class=""><span>3</span> Monetize your designs </h3>
+                        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus veritatis minima ipsam. Accusantium eum, debitis vero laudantium officiis ut eligendi odio, corporis dolores dignissimos, repudiandae dolorum itaque facere magnam. Sapiente!</p>
+                    </div>
+                </div>
+        </main>
     </header>
 </body>
 </html>
